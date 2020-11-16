740ba1d71431c4dc8633a810e48aabbb682610f1
Arash Izadi <arash.iz@live.com>
2020-11-16 01:51:56 -0800

    Bolt 103 Done

---
+++ IMSD_F20_Intervention_Design/Assets/Tutorial/Scripts/CubeBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+public class CubeBehaviour : Bolt.EntityBehaviour<ICubeState>
+{
+    public override void Attached()
+    {
+        state.SetTransforms(state.CubeTransform, transform);
+
+        if (entity.IsOwner)
+        {
+            state.CubeColor = new Color(Random.value, Random.value, Random.value);
+        }
+
+        state.AddCallback("CubeColor", ColorChanged);
+    }
+
+    public override void SimulateOwner()
+    {
+        var speed = 4f;
+        var movement = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) { movement.z += 1; }
+        if (Input.GetKey(KeyCode.S)) { movement.z -= 1; }
+        if (Input.GetKey(KeyCode.A)) { movement.x -= 1; }
+        if (Input.GetKey(KeyCode.D)) { movement.x += 1; }
+
+        if (movement != Vector3.zero)
+        {
+            transform.position = transform.position + (movement.normalized * speed * BoltNetwork.FrameDeltaTime);
+        }
+    }
+
+    void OnGUI()
+    {
+        if (entity.IsOwner)
+        {
+            GUI.color = state.CubeColor;
+            GUILayout.Label("@@@");
+            GUI.color = Color.white;
+        }
+    }
+
+    void ColorChanged()
+    {
+        GetComponent<Renderer>().material.color = state.CubeColor;
+    }
+}
