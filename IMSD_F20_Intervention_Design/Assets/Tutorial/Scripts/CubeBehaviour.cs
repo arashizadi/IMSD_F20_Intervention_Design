eb5fb2ce493cb8b2e8580da2e77f122a242d3465
Arash Izadi <arash.iz@live.com>
2020-11-16 02:07:35 -0800

    Bolt 104 Done

--- IMSD_F20_Intervention_Design/Assets/Tutorial/Scripts/CubeBehaviour.cs
+++ IMSD_F20_Intervention_Design/Assets/Tutorial/Scripts/CubeBehaviour.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Bolt;
 
-public class CubeBehaviour : Bolt.EntityBehaviour<ICubeState>
+public class CubeBehaviour : Bolt.EntityEventListener<ICubeState>
 {
+    private float _resetColorTime;
+    private Renderer _renderer;
+
     public override void Attached()
     {
+        _renderer = GetComponent<Renderer>();
+
         state.SetTransforms(state.CubeTransform, transform);
 
         if (entity.IsOwner)
@@ -30,6 +35,27 @@ public class CubeBehaviour : Bolt.EntityBehaviour<ICubeState>
         if (movement != Vector3.zero)
         {
             transform.position = transform.position + (movement.normalized * speed * BoltNetwork.FrameDeltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            var flash = FlashColorEvent.Create(entity);
+            flash.FlashColor = Color.red;
+            flash.Send();
+        }
+    }
+
+    public override void OnEvent(FlashColorEvent evnt)
+    {
+        _resetColorTime = Time.time + 0.2f;
+        _renderer.material.color = evnt.FlashColor;
+    }
+
+    void Update()
+    {
+        if (_resetColorTime < Time.time)
+        {
+            _renderer.material.color = state.CubeColor;
         }
     }
 
--- IMSD_F20_Intervention_Design/Assets/Tutorial/Scripts/NetworkCallbacks.cs
+++ IMSD_F20_Intervention_Design/Assets/Tutorial/Scripts/NetworkCallbacks.cs
@@ -1,15 +1,39 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
 
 [BoltGlobalBehaviour]
-public class NetworkCallbacks : Bolt.GlobalEventListener
+public class NetworkCallbacks : GlobalEventListener
 {
+    List<string> logMessages = new List<string>();
+
+    void OnGUI()
+    {
+        // only display max the 5 latest log messages
+        int maxMessages = Mathf.Min(5, logMessages.Count);
+
+        GUILayout.BeginArea(new Rect(Screen.width / 2 - 200, Screen.height - 100, 400, 100), GUI.skin.box);
+
+        for (int i = 0; i < maxMessages; ++i)
+        {
+            GUILayout.Label(logMessages[i]);
+        }
+
+        GUILayout.EndArea();
+    }
+
     public override void SceneLoadLocalDone(string scene)
     {
         // randomize a position
-        var spawnPosition = new Vector3(Random.Range(-16, 16), 0, Random.Range(-16, 16));
+        var spawnPosition = new Vector3(Random.Range(-8, 8), 0, Random.Range(-8, 8));
 
         // instantiate cube
         BoltNetwork.Instantiate(BoltPrefabs.Cube, spawnPosition, Quaternion.identity);
+    }
+
+    public override void OnEvent(LogEvent evnt)
+    {
+        logMessages.Insert(0, evnt.Message);
     }
 }
